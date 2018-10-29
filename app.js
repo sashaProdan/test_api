6d586d1b0dbecd5dc79986f51f9166185a19503e
Nick Calibey <nick.calibey@gmail.com>
2018-10-29 00:03:28 -0500

    added PostgreSQL DB

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv').config()
+
 let bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
@@ -9,6 +11,7 @@ let apiRouter = require('./routes/api');
 
 var app = express();
 
+app.use(bodyParser.text());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var client = require('../connection');
 
 /* GET users listing. */
 router.get('/events', function(req, res, next) {
+  client.query('SELECT * FROM testing', (err, res) => {
+    console.log(err ? err.stack : res.rows[0]) // Hello World!
+  });
   res.send([]);
 });
 
